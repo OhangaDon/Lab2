8ef7768deda8c7314e0fda848ce83aa0ed9557df
ohanga <ohanga@ohanga.ohanga>
2024-03-27 23:48:55 +0300

    initial commit

---
+++ AverageMark/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnComputeGrade = New System.Windows.Forms.Button()
+        Me.txtCalculus = New System.Windows.Forms.TextBox()
+        Me.txtObjectO = New System.Windows.Forms.TextBox()
+        Me.txtC = New System.Windows.Forms.TextBox()
+        Me.txtDiscrete = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCancel
+        '
+        Me.btnCancel.BackColor = System.Drawing.Color.DarkGreen
+        Me.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnCancel.Location = New System.Drawing.Point(678, 481)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(173, 51)
+        Me.btnCancel.TabIndex = 23
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = False
+        '
+        'btnComputeGrade
+        '
+        Me.btnComputeGrade.BackColor = System.Drawing.Color.DarkGreen
+        Me.btnComputeGrade.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnComputeGrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnComputeGrade.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnComputeGrade.Location = New System.Drawing.Point(222, 493)
+        Me.btnComputeGrade.Name = "btnComputeGrade"
+        Me.btnComputeGrade.Size = New System.Drawing.Size(161, 48)
+        Me.btnComputeGrade.TabIndex = 22
+        Me.btnComputeGrade.Text = "Compute Grade"
+        Me.btnComputeGrade.UseVisualStyleBackColor = False
+        '
+        'txtCalculus
+        '
+        Me.txtCalculus.Location = New System.Drawing.Point(362, 380)
+        Me.txtCalculus.Name = "txtCalculus"
+        Me.txtCalculus.Size = New System.Drawing.Size(222, 26)
+        Me.txtCalculus.TabIndex = 21
+        '
+        'txtObjectO
+        '
+        Me.txtObjectO.Location = New System.Drawing.Point(362, 315)
+        Me.txtObjectO.Name = "txtObjectO"
+        Me.txtObjectO.Size = New System.Drawing.Size(222, 26)
+        Me.txtObjectO.TabIndex = 20
+        '
+        'txtC
+        '
+        Me.txtC.Location = New System.Drawing.Point(362, 251)
+        Me.txtC.Name = "txtC"
+        Me.txtC.Size = New System.Drawing.Size(222, 26)
+        Me.txtC.TabIndex = 19
+        '
+        'txtDiscrete
+        '
+        Me.txtDiscrete.Location = New System.Drawing.Point(362, 184)
+        Me.txtDiscrete.Name = "txtDiscrete"
+        Me.txtDiscrete.Size = New System.Drawing.Size(222, 26)
+        Me.txtDiscrete.TabIndex = 18
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(217, 116)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(216, 29)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Enter your marks:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(191, 384)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(79, 22)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Calculus"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(191, 315)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 22)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "OOP"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(191, 252)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 22)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "C++"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(191, 188)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 22)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Discrete"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Green
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(409, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(319, 46)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Grading System"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ClientSize = New System.Drawing.Size(1043, 587)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnComputeGrade)
+        Me.Controls.Add(Me.txtCalculus)
+        Me.Controls.Add(Me.txtObjectO)
+        Me.Controls.Add(Me.txtC)
+        Me.Controls.Add(Me.txtDiscrete)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnComputeGrade As Button
+    Friend WithEvents txtCalculus As TextBox
+    Friend WithEvents txtObjectO As TextBox
+    Friend WithEvents txtC As TextBox
+    Friend WithEvents txtDiscrete As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ AverageMark/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub btnComputeGrade_Click(sender As Object, e As EventArgs) Handles btnComputeGrade.Click
+        Dim num1, num2, num3, num4, total, average As Integer
+
+        If Integer.TryParse(txtCalculus.Text, num1) And Integer.TryParse(txtObjectO.Text, num2) And Integer.TryParse(txtDiscrete.Text, num3) And Integer.TryParse(txtC.Text, num4) Then
+            If num1 > 100 Or num2 > 100 Or num3 > 100 Or num4 > 100 Then
+                MessageBox.Show("Marks per subject cannot exceed 100!")
+
+            Else
+                total = num1 + num2 + num3 + num4
+                average = total / 4
+
+                If average <= 49 Then
+                    MessageBox.Show("You get a Fail")
+
+                ElseIf average <= 64 Then
+                    MessageBox.Show("Mean Grade = C")
+
+                ElseIf average <= 74 Then
+                    MessageBox.Show("Mean Grade = B")
+
+                ElseIf average >= 75 And average <= 100 Then
+                    MessageBox.Show("Mean Grade = A")
+
+                End If
+
+            End If
+        Else
+            MessageBox.Show("Invalid data input! Please enter correct values.")
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        'Quit the program
+        Application.Exit()
+    End Sub
+
+End Class
